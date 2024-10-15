b2f8df983497fe82896165a0bbb19c613a418dc5
daimonpei <d.jumpei@gmail.com>
2024-10-15 11:10:14 +0900

    調整

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     head: {
       title: 'front_nuxt_corporate',
       htmlAttrs: {
-        lang: 'en'
+        lang: 'ja'
       },
       meta: [
         { charset: 'utf-8' },
